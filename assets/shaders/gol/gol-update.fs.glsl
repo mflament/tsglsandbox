a1862e931ae84cd0afca499f1bdfcac53b48701b
Marc <marcflament@free.fr>
2021-05-23 23:39:19 +0200

    gol world repeat

--- assets/shaders/gol/gol-update.fs.glsl
+++ assets/shaders/gol/gol-update.fs.glsl
@@ -1,6 +1,8 @@
 #version 300 es
 precision mediump float;
 
+//#define REPEAT false
+
 #define TOP_LEFT ivec2(-1, 1)
 #define TOP ivec2(0, 1)
 #define TOP_RIGHT ivec2(1, 1)
@@ -21,22 +23,37 @@ uniform uint[18] states_matrix;
 in vec2 texcoord;
 out vec4 color;
 
-int countNeighbors(ivec2 pos) {
-  float n = texelFetch(data, pos + TOP_LEFT, 0).r;
-  n += texelFetch(data, pos + TOP, 0).r;
-  n += texelFetch(data, pos + TOP_RIGHT, 0).r;
-  n += texelFetch(data, pos + RIGHT, 0).r;
-  n += texelFetch(data, pos + BOTTOM_RIGHT, 0).r;
-  n += texelFetch(data, pos + BOTTOM, 0).r;
-  n += texelFetch(data, pos + BOTTOM_LEFT, 0).r;
-  n += texelFetch(data, pos + LEFT, 0).r;
-  return int(n);
+int getNeighbor(ivec2 pos, ivec2 dataSize) {
+#ifdef REPEAT
+  if (pos.x < 0)
+    pos.x = dataSize.x + pos.x;
+  if (pos.x >= dataSize.x)
+    pos.x = pos.x - dataSize.x;
+  if (pos.y < 0)
+    pos.y = dataSize.y + pos.y;
+  if (pos.y >= dataSize.y)
+    pos.y = pos.y - dataSize.y;
+#endif
+  return int(texelFetch(data, pos, 0).r);
+}
+
+int countNeighbors(ivec2 pos, ivec2 dataSize) {
+  int n = getNeighbor(pos + TOP_LEFT, dataSize);
+  n += getNeighbor(pos + TOP, dataSize);
+  n += getNeighbor(pos + TOP_RIGHT, dataSize);
+  n += getNeighbor(pos + RIGHT, dataSize);
+  n += getNeighbor(pos + BOTTOM_RIGHT, dataSize);
+  n += getNeighbor(pos + BOTTOM, dataSize);
+  n += getNeighbor(pos + BOTTOM_LEFT, dataSize);
+  n += getNeighbor(pos + LEFT, dataSize);
+  return n;
 }
 
 void main() {
   ivec2 pos = ivec2(gl_FragCoord);
   int state = int(texelFetch(data, pos, 0).r);
-  int neighbors = countNeighbors(pos);
+  ivec2 dataSize = textureSize(data, 0);
+  int neighbors = countNeighbors(pos, dataSize);
   uint nextState = states_matrix[state * 9 + neighbors];
   color = vec4(float(nextState));
 }
